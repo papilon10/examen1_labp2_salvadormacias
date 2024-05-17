7934a99f035103764a4ca6fdffa17952529851a3
papilon10 <140213599+papilon10@users.noreply.github.com>
2024-05-17 14:04:15 -0600

    agregar enemigo creado

--- src/main/java/www/examen1_labp2_salvadormacias/Examen1_Labp2_SalvadorMacias.java
+++ src/main/java/www/examen1_labp2_salvadormacias/Examen1_Labp2_SalvadorMacias.java
@@ -87,4 +87,70 @@ public class Examen1_Labp2_SalvadorMacias {
 
     }//fin agregar spiderman
 
+    public static void agregar_enemigo() {
+        System.out.println("---agregar enemigo---");
+        boolean menu_agg_enemigo = false;
+        do {
+            System.out.println("1. agregar alieligena");
+            System.out.println("2. agregar alterado");
+            System.out.println("3. agregar blindado");
+            System.out.println("4. salir del submenu");
+            int opc_agg_enemigo = lea.nextInt();
+            switch (opc_agg_enemigo) {
+                case 1: {
+                    System.out.println("Ingrese el nombre: ");
+                    String nombre = str.nextLine();
+                    System.out.println("Ingrese los puntos de vida: ");
+                    double hp = lea.nextDouble();
+                    System.out.println("Ingrese los puntos de ataque: ");
+                    double hitpoints = lea.nextDouble();
+                    System.out.println("Ingrese el nombre del universo: ");
+                    String universo = lea.nextLine();
+                    lista_enemigo.add(new alieligena(nombre, hp, hitpoints, universo));
+                    System.out.println("enemigo creado exitosamente...");
+                }
+
+                break;
+                case 2: {
+                    System.out.println("Ingrese el nombre: ");
+                    String nombre = str.nextLine();
+                    System.out.println("Ingrese los puntos de vida: ");
+                    double hp = lea.nextDouble();
+                    System.out.println("Ingrese los puntos de ataque: ");
+                    double hitpoints = lea.nextDouble();
+                    System.out.println("Ingrese el nombre del universo: ");
+                    String universo = lea.nextLine();
+                    lista_enemigo.add(new alterado(nombre, hp, hitpoints, universo));
+                    System.out.println("enemigo creado exitosamente...");
+
+                }
+                break;
+                case 3: {
+                    System.out.println("Ingrese el nombre: ");
+                    String nombre = str.nextLine();
+                    System.out.println("Ingrese los puntos de vida: ");
+                    double hp = lea.nextDouble();
+                    System.out.println("Ingrese los puntos de ataque: ");
+                    double hitpoints = lea.nextDouble();
+                    System.out.println("Ingrese el nombre del universo: ");
+                    String universo = lea.nextLine();
+                    lista_enemigo.add(new blindado(nombre, hp, hitpoints, universo));
+                    System.out.println("enemigo creado exitosamente...");
+
+                }
+                break;
+                case 4: {
+                    menu_agg_enemigo = true;
+                    System.out.println("se abandonara el submenu...");
+
+                }
+                break;
+                default:
+                    System.out.println("opcion ingresada es invalida...");
+            }
+
+        } while (menu_agg_enemigo != true);
+
+    }//fin agregar enemigo
+
 }//fin clase
